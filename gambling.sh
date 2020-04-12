52a8eb86a3f482bc1d4342aa483fad6a90518747
sampada8217 <sampadatirthkar@gmail.com>
2020-04-12 23:15:49 +0530

    Added code for checking win or loose

--- gambling.sh
+++ gambling.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 STAKE_EVERYDAY=100
 STAKE_START=1
-
 echo "Enter n of trials"
 read n
 goal=1
 bets=0
 wins=0
 
+random=$((RANDOM%2))
+if [  $random -le  0 ]
+then
+     echo "Win  $1"
+else
+     echo "Loose $1"
+fi
+	
 
