da0ddf22db4f4f72905dba58ebeface6fb09fcc6
sampada8217 <sampadatirthkar@gmail.com>
2020-04-13 00:13:11 +0530

    Added Code for calculating won or loose 50 percent of stake and resign

--- gambling.sh
+++ gambling.sh
@@ -1,18 +1,43 @@
 #!/bin/bash -x
-STAKE_EVERYDAY=100
+STAKE=100
 STAKE_START=1
 echo "Enter n of trials"
 read n
-goal=1
+goal=100
 bets=0
 wins=0
+for (( i=0; i<n; i++ ))
+do
+        cash=$STAKE_START
+	while [ $cash -gt 0 ] && [ $cash -lt  $goal ] 
+        do
+		((bets++))
+		random=$((RANDOM%2))
+		if [  $random -lt  1 ]
+		then 
+                      ((cash++))
+		else
+     		      ((cash--))
+		fi
+	done
 
-random=$((RANDOM%2))
-if [  $random -le  0 ]
+       	if [ $cash -eq $goal ]
+ 	then
+		((wins++))
+        else
+                ((loose++))
+   	fi
+done
+echo  $wins   "   " $n
+percent=$(( 100 * $wins / $n ))
+echo "Percent of games won" $percent
+percent1=$(( 100 * $loose / $n ))
+echo "Percent of games loose" $percent1
+
+if [ $percent -ge 50 ]
 then
-     echo "Win  $1"
-else
-     echo "Loose $1"
+     echo "Resign for the day"
+elif [ $percent1 -ge  50 ]
+then
+     echo "Resign for th day"
 fi
-	
-
