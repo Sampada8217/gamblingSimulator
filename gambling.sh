5fc5ac40d53d61d6c193720b6a09ada2e0d85b83
sampada8217 <sampadatirthkar@gmail.com>
2020-04-16 15:48:15 +0530

    Refactored Code(followed naming convention,funtion used)

--- gambling.sh
+++ gambling.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 STAKE=100
 STAKE_START=1
+GOAL=100
+function gambling()
+{
 echo "Enter n of trials"
 read n
-goal=100
 bets=0
 wins=0
 loose=0
@@ -14,7 +16,7 @@ do
         cash=$STAKE_START
         while [ $count -ne  30 ]
         do
-		while [ $cash -gt 0 ] && [ $cash -lt  $goal ]  
+		while [ $cash -gt 0 ] && [ $cash -lt  $GOAL ]    #cash should be less than 100 which is goal for a day  
        		do
 			((bets++))
 			random=$((RANDOM%2))
@@ -24,25 +26,29 @@ do
 			else	
      		      		((cash--))
 			fi 
+                        if [ $cash -eq $GOAL ]
+                        then
+                            ((wins++))
+                        else
+                            ((loose++))
+                        fi
                 done
                 amount=$(( $cash + $amount ))
         	((count++))
-        done
-                if [ $cash -eq $goal ]
-   	        then
-               	        ((wins++))
-        	else
-               		((loose++)) 	
-   	 	fi
+        
+                wins=$wins
+                loose=$loose 
+                
+       done
 done
-
+#Total amount of won and lost
 echo  $wins  " " $n 
 echo  $loose
-
+#by how much loose or win
 diff=$(( $wins - $loose ))
-
+#percent of wins
 percent=$(( $wins / $n * 100 ))
- 
+#if win is greater than 50 for a month continue else stop playing
 if [ $percent -ge 50 ]
 then
      echo "Continue Playing"
@@ -50,4 +56,5 @@ elif [ $percent -le  50 ]
 then
      echo "Resign for the day"
 fi
-
+}
+gamblingGame="$( gambling )" 
